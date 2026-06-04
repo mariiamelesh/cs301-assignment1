23c5a0e1976b400e2b4abbc8eb2c984e75d8688b
skibidi <mariia.melesh.25@kse.org.ua>
2026-06-04 12:08:49 +0300

    updated query to use big_organizations cte to filter out small organizations with <10 users

--- query.sql
+++ query.sql
@@ -2,32 +2,32 @@ with user_amount_by_organization as (
 	select org_id, count(user_id) as user_count 
 	from activity.users 
 	group by org_id
-	having count(user_id) > 1
+	having count(user_id) > 10
 ), big_organizations as (
 	select organizations.org_id, industry, country
 	from activity.organizations
 	where organizations.org_id in (select org_id from user_amount_by_organization)
 ), failed_logins as (
-	select users.user_id, role, organizations.industry, count(login_logs) as failed_login_count
+	select users.user_id, role, big_organizations.industry, count(login_logs) as failed_login_count
 	from activity.login_logs
 	inner join activity.users on login_logs.user_id = users.user_id
-	inner join activity.organizations on users.org_id = organizations.org_id
+	inner join big_organizations on users.org_id = big_organizations.org_id
 	where login_logs.status = 'Failed'
-	group by users.user_id, organizations.industry
+	group by users.user_id, big_organizations.industry
 ), system_failures as (
-	(select organizations.industry, systems.os_type, count(incident_id) as incident_count
+	(select big_organizations.industry, systems.os_type, count(incident_id) as incident_count
 	from activity.security_incidents
-	inner join activity.organizations on security_incidents.org_id = organizations.org_id
-	inner join activity.systems on organizations.org_id = systems.org_id
+	inner join big_organizations on security_incidents.org_id = big_organizations.org_id
+	inner join activity.systems on big_organizations.org_id = systems.org_id
 	where severity = 'Critical'
-	group by organizations.org_id, organizations.industry, systems.os_type)
+	group by big_organizations.org_id, big_organizations.industry, systems.os_type)
 	union all
-	(select organizations.industry, systems.os_type, count(event_id) as incident_count
+	(select big_organizations.industry, systems.os_type, count(event_id) as incident_count
 	from activity.network_events
 	inner join activity.systems on network_events.system_id = systems.system_id
-	inner join activity.organizations on systems.org_id = organizations.org_id
+	inner join big_organizations on systems.org_id = big_organizations.org_id
 	where severity = 'Critical'
-	group by organizations.org_id, organizations.industry, systems.os_type)
+	group by big_organizations.org_id, big_organizations.industry, systems.os_type)
 )
 
 select *
